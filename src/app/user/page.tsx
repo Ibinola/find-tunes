273fcaeb2580aa16b089b02e2f501a786d4de318
Ibinola <ibinolamichael1@gmail.com>
2024-07-21 19:39:52 +0100

    fetch artist and album data

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -14,6 +14,14 @@ import {
   FormLabel,
   FormMessage
 } from "@/components/ui/form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import { useState, useEffect } from "react";
 import { z } from "zod"
 
@@ -24,6 +32,7 @@ export default function GetAlbum() {
 
   const [accessToken, setAccessToken] = useState()
   const [loading, setLoading] = useState(false)
+  const [albums, setAlbums] = useState([])
 
   // VALIDATE FORM
   const albumFormSchema = z.object({
@@ -59,11 +68,11 @@ export default function GetAlbum() {
   }, [clientId, clientSecret]);
 
 
-  // GET ALBUM, USING ACCESS TOKEN
+  // GET ARTIST, USING ACCESS TOKEN
   const search = async (album: z.infer<typeof albumFormSchema>) => {
     setLoading(true);
     try {
-      let albumParams = {
+      let artistParams = {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -71,57 +80,74 @@ export default function GetAlbum() {
         },
       };
 
-      const response = await fetch(`https://api.spotify.com/v1/search?q=${album.album_name}`&typep);
-      const data = await response.json();
+      const response = await fetch(`https://api.spotify.com/v1/search?q=${album.album_name}&type=artist`, artistParams);
+      const artistData = await response.json();
+      // setAlbums(data.artists.items[0].id);
+      const artistId = artistData.artists.items[0].id
+      console.log(album.album_name)
+      console.log(artistId)
+
+      const responseAlbum = await fetch(`https://api.spotify.com/v1/artists/${artistId}/albums?include_groups=album`, artistParams)
+      const albumData = await responseAlbum.json();
+      const albumArray = albumData.items
+      setAlbums(albumArray)
+      console.log(albumArray)
       setLoading(false);
-      // console.log(data);
-      console.log(data)
-      console.log(data.albums.items)
       form.reset()
     } catch (error) {
       setLoading(false);
-
     }
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black">
-      <div className="text-center space-y-6">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(search)}
-            className="w-2/3 space-y-6"
-          >
-            <FormField
-              //   control={form}
-              name="album_name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Get Album</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Search for an Artist" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button
-              type="submit"
-              size={"lg"}
-            // className="text-black-400 hover:no-underline px-5 py-5 bg-transparent text-white"
-            >
-              {loading ? (
-                <>
-                  <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-                  Searching
-                </>
-              ) : (
-                'Search'
-              )}
-            </Button>
-          </form>
-        </Form>
+    <div className="flex min-h-screen items-center justify-center bg-black text-white">
+      <div className="w-full max-w-lg p-8 space-y-8">
+        <div className="">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(search)} className="space-y-6">
+              <FormField
+                name="album_name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Get Album</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Search for an Artist" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" size="lg">
+                {loading ? (
+                  <>
+                    <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                    Searching
+                  </>
+                ) : (
+                  'Search'
+                )}
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="flex flex-col items-center space-y-4">
+          {albums.map((album, index) => (
+            <>
+              <Card key={index} className="w-full bg-gray-800 text-white p-4">
+                <CardHeader>
+                  {album.name}
+                </CardHeader>
+              </Card>
+
+
+              <Card>
+                {/* <img src={album.images[0]} alt="" /> */}
+              </Card>
+            </>
+          ))}
+        </div>
       </div>
-    </div >
+    </div>
+
   );
 }
