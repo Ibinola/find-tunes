706ec5000550e66f5765c0fb8663c067953b0835
Ibinola <ibinolamichael1@gmail.com>
2024-07-21 14:50:15 +0100

    feat: get albums

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
     <main className="flex min-h-screen items-center justify-center bg-black text-white">
       <div className="text-center space-y-6">
         <h1 className="text-4xl font-bold">
-          find <span className="italic">tunes</span>
+          find <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-[#1db954] relative inline-block">
+            <span className="relative text-white">tunes</span>
+          </span>
         </h1>
         {/* <Image
           src="/music-note.svg"
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
+import { SpotifyApi } from "@spotify/web-api-ts-sdk";
 import { Button } from "@/components/ui/button";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { ReloadIcon } from "@radix-ui/react-icons"
 import { useForm } from "react-hook-form";
 import {
   Form,
@@ -10,56 +12,116 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage
 } from "@/components/ui/form";
+import { useState, useEffect } from "react";
+import { z } from "zod"
+
 
 export default function GetAlbum() {
-  //   const formSchema = z.object({
-  //     username: z.string().min(2, {
-  //       message: "Username must be at least 2 characters.",
-  //     }),
-  //   });
-  const form = useForm({
-    // resolver: zodResolver(),
-    defaultValues: {
-      username: "",
-    },
-  });
+  const clientId = process.env.NEXT_PUBLIC_CLIENT_ID
+  const clientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET
 
-  function handleLookUp(values: any) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
-  }
+  const [accessToken, setAccessToken] = useState()
+  const [loading, setLoading] = useState(false)
+
+  // VALIDATE FORM
+  const albumFormSchema = z.object({
+    album_name: z.string().max(50, {
+      message: 'Enter a valid album name'
+    }),
+  })
+
+  const form = useForm<z.infer<typeof albumFormSchema>>({
+    resolver: zodResolver(albumFormSchema),
+    defaultValues: {
+      album_name: "",
+    },
+  })
+
+
+  // GET ACCESS TOKEN
+  useEffect(() => {
+    let authParams = {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/x-www-form-urlencoded'
+      },
+      body:
+        `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+    };
+
+    fetch("https://accounts.spotify.com/api/token", authParams)
+      .then(result => result.json())
+      .then(data => {
+        setAccessToken(data.access_token);
+      });
+  }, [clientId, clientSecret]);
+
+
+  // GET ALBUM, USING ACCESS TOKEN
+  const search = async (album: z.infer<typeof albumFormSchema>) => {
+    setLoading(true);
+    try {
+      let albumParams = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+
+      const response = await fetch(`https://api.spotify.com/v1/search?q=${album.album_name}`&typep);
+      const data = await response.json();
+      setLoading(false);
+      // console.log(data);
+      console.log(data)
+      console.log(data.albums.items)
+      form.reset()
+    } catch (error) {
+      setLoading(false);
+
+    }
+  };
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-black">
       <div className="text-center space-y-6">
         <Form {...form}>
           <form
-            onSubmit={form.handleSubmit(handleLookUp)}
+            onSubmit={form.handleSubmit(search)}
             className="w-2/3 space-y-6"
           >
             <FormField
               //   control={form}
-              name="username"
+              name="album_name"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Get Album</FormLabel>
                   <FormControl>
-                    <Input placeholder="Enter an Album Name" {...field} />
+                    <Input placeholder="Search for an Artist" {...field} />
                   </FormControl>
-                  {/* <FormMessage /> */}
+                  <FormMessage />
                 </FormItem>
               )}
             />
             <Button
               type="submit"
-              //   className="text-black-400 hover:no-underline px-5 py-5 bg-transparent text-white"
+              size={"lg"}
+            // className="text-black-400 hover:no-underline px-5 py-5 bg-transparent text-white"
             >
-              Get Album
+              {loading ? (
+                <>
+                  <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                  Searching
+                </>
+              ) : (
+                'Search'
+              )}
             </Button>
           </form>
         </Form>
       </div>
-    </div>
+    </div >
   );
 }
